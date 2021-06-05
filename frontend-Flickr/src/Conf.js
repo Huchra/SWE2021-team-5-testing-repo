57a6a5371010eb60bd14e16c40ba24dd41e62c0c
Ziyad <17732483+ziyadss@users.noreply.github.com>
2021-06-05 16:53:00 +0200

    another try at integration

--- frontend-Flickr/src/Conf.js
+++ frontend-Flickr/src/Conf.js
@@ -3,7 +3,7 @@ export default {
   apiURL: 'http://127.0.0.1:8000',
   localURL: 'http://localhost:5000/',
   LocalNoPath: 'http://localhost:5000',
-  backURL: 'http://127.0.0.1:8000/api/',
+  backURL: 'http://lvh.me:8000/api/',
   productionURL: 'http://back:8000/api/',
 
 };
